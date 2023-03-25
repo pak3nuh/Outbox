99606d757794b778d2392541c7361ececa3a6040
Nuno Caro <nuno.pik@gmail.com>
2023-03-25 12:11:44 +0000

    EntryStorage tests (WIP)

--- integration-test/build.gradle.kts
+++ integration-test/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     testImplementation("org.testcontainers:junit-jupiter:1.17.6")
     testImplementation(Dependencies.slf4jSimple)
     testImplementation(project(":lock"))
+    testImplementation(project(":impl"))
     testImplementation(project(":util"))
     testImplementation("org.postgresql:postgresql:42.6.0")
     testImplementation("mysql:mysql-connector-java:8.0.32")
---
+++ integration-test/src/test/kotlin/io/github/pak3nuh/messaging/outbox/storage/PostgresEntryStorageTest.kt
@@ -0,0 +1,13 @@
+package io.github.pak3nuh.messaging.outbox.storage
+
+import org.junit.jupiter.api.Test
+import org.testcontainers.junit.jupiter.Testcontainers
+
+@Testcontainers
+class PostgresEntryStorageTest {
+
+    @Test
+    fun `should store entity`() {
+        TODO()
+    }
+}
